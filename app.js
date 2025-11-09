88cc2ab80c023b9805ee55da1ffc3335a15382cd
Adalid Martinez <adalvarez2021@gmail.com>
2025-11-09 10:20:22 +0100

    1. restructuracion de proyecto

---
+++ app.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll(".nav-menu a");
+  const sections = document.querySelectorAll(".section");
+  const hamburguer = document.getElementById("hamburguer");
+  const navMenu = document.getElementById("nav-menu");
+
+  //Navegacion SPA entre secciones
+  links.forEach(link => {
+    link.addEventListener("click", e => {
+      e.preventDefault();
+      const target = link.dataset.section;
+
+      // Actualiza los estados activos
+      links.forEach(l => l.classList.remove("active"));
+      link.classList.add("active");
+
+      sections.forEach(sec => {
+        sec.classList.remove("active");
+        if (sec.id === target) sec.classList.add("active");
+      });
+
+      //Cerrar menu hamburguesa
+      navMenu.classList.remove("show");
+      hamburguer.classList.remove("active");
+    });
+  });
+
+  //Toggle menu hamburguesa
+  hamburguer.addEventListener("click", ()=>{
+    navMenu.classList.toggle("show");
+    hamburguer.classList.toggle("active");
+  });
+});
